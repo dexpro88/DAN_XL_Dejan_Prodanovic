161de9e80fbb3678d93cbd7383eae4ed5370c9c7
dexpro988 <dejan.prodanovic@panonit.com>
2020-06-26 05:33:25 -0700

    Added class Printing

---
+++ DAN_XL_Dejan_Prodanovic/DAN_XL_Dejan_Prodanovic/Printing.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DAN_XL_Dejan_Prodanovic
+{
+    class Printing
+    {
+        bool[] allComputersPrinted = new bool[10];
+        object lock1 = new object();
+        object lock2 = new object();
+
+        Random rnd = new Random();
+
+        Thread[] threads = new Thread[10];
+        bool endOfProgam = false;
+    }
+}
